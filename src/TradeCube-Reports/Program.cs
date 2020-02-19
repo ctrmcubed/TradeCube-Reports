0444a5941e4dafbb3fe6e5063a29c979566a4418
Andy Bennett <bennean@gmail.com>
2020-02-19 10:57:38 +0000

    Moved to generic host builder.

--- src/TradeCube-Reports/Program.cs
+++ src/TradeCube-Reports/Program.cs
@@ -1,5 +1,5 @@
-using Microsoft.AspNetCore;
 using Microsoft.AspNetCore.Hosting;
+using Microsoft.Extensions.Hosting;
 using Microsoft.Extensions.Logging;
 using NLog.Web;
 using TradeCube_Reports.Helpers;
@@ -10,31 +10,32 @@ namespace TradeCube_Reports
     {
         public static void Main(string[] args)
         {
-            BuildWebHost(args).Run();
+            CreateHostBuilder(args)
+                .Build()
+                .Run();
         }
 
-        private static IWebHost BuildWebHost(string[] args) =>
-            WebHost.CreateDefaultBuilder(args)
-                .UseStartup<Startup>()
-                .ConfigureKestrel(o =>
+        private static IHostBuilder CreateHostBuilder(string[] args) =>
+            Host.CreateDefaultBuilder(args)
+                .ConfigureWebHostDefaults(webBuilder =>
                 {
-                    var port = EnvironmentVariableHelper.GetIntEnvironmentVariable("TRADECUBE_REPORTS_HTTPS_PORT");
-                    var certificateInfo = X509Helper.CertificateInfo("TRADECUBE_REPORTS_CERT_NAME", "TRADECUBE_REPORTS_CERT_PASSWORD");
+                    webBuilder.ConfigureKestrel(o =>
+                        {
+                            var port = EnvironmentVariableHelper.GetIntEnvironmentVariable("TRADECUBE_REPORTS_HTTPS_PORT");
+                            var certificateInfo = X509Helper.CertificateInfo("TRADECUBE_REPORTS_CERT_NAME", "TRADECUBE_REPORTS_CERT_PASSWORD");
 
-                    if (X509Helper.IsValidHttpsConfig(port, certificateInfo))
-                    {
-                        o.ListenAnyIP(port ?? 0, options =>
-                          {
-                              options.UseHttps(certificateInfo.name, certificateInfo.password);
-                          });
-                    }
-                })
-                .ConfigureLogging(logging =>
-                {
-                    logging.ClearProviders();
-                    logging.SetMinimumLevel(LogLevel.Trace);
-                })
-                .UseNLog()
-                .Build();
+                            if (X509Helper.IsValidHttpsConfig(port, certificateInfo))
+                            {
+                                o.ListenAnyIP(port ?? 0, options => { options.UseHttps(certificateInfo.name, certificateInfo.password); });
+                            }
+                        })
+                        .UseStartup<Startup>()
+                        .ConfigureLogging(logging =>
+                        {
+                            logging.ClearProviders();
+                            logging.SetMinimumLevel(LogLevel.Trace);
+                        })
+                        .UseNLog();
+                });
     }
 }
--- src/TradeCube-Reports/Startup.cs
+++ src/TradeCube-Reports/Startup.cs
@@ -13,12 +13,12 @@ namespace TradeCube_Reports
 {
     public class Startup
     {
+        private IConfiguration configuration;
+
         public Startup(IConfiguration configuration)
         {
-            Configuration = configuration;
+            this.configuration = configuration;
         }
-
-        public IConfiguration Configuration { get; }
 
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
